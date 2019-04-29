94b60b54cf8dc1d5a058d0c4719eb6b7f7597adf
sam osborne <samosborne121@gmail.com>
2019-04-29 06:38:47 +0000

    finished

--- file 1.c
+++ file 1.c
@@ -3,13 +3,16 @@
 
 /* 
 rotation cipher encryption
+encrypts your massege using a rotion method when given a key. the key determins the amount the rotion will increase or decreas by
+giving an output in encrypted text.
 */
 
 int main()
 
 {
+    printf ("welcome to the place to decrypt and encrypte your secret messages\n");
     int pk=0;
-printf ("enter 1 to encrypt roataion cipher with key\n enter 2 to decrypt roatation cyper with key\n");
+printf ("enter 1 to encrypt roataion cipher with key\nenter 2 to decrypt roatation cyper with key\n");
 scanf ("%d", &pk);
 if (pk==1)
 {
@@ -24,15 +27,18 @@ if (pk==1)
 	printf("enter key:\n");
 	scanf(" %d", &key);
     printf("\n");
-	for(i=0; text[i] != '\0'; ++i) { 
-		num = text[i]; 
-		if(num >= 65 && num <= 90) { 
-			num = num + key; 
-			if(num < 65) { 
+	for(i=0; text[i] != '\0'; ++i) //for every charactor written it will do the following 
+	{ 
+		num = text[i]; //converts text into an integer 
+		if(num >= 65 && num <= 90) //only asses text leaving out spaces, full stops, etc,
+		{ 
+			num = num + key; //increses or decreases the ascii code by the key (the rotation)
+			if(num < 65) //if ascii is less then 65 (A) it is increased by 26 (the distance between A and Z) so that the code only rotates around points 65 and 90
+			{ 
 			num = num + 26;
 			}
 			if(num > 90) { 
-				num = num - 26; 
+				num = num - 26; //if the aski is grater then 90 then it is - by 26
 			}
 			
 			text[i] = num; 
@@ -44,6 +50,10 @@ if (pk==1)
 	return 0;
 }
 
+/*
+ decryption rotaion cipher
+ decrypts and incripted messaeg when given a key by adding or subtraction the given key so that it can output the decypted messaage
+ */
 else if (pk==2)
 {
 
@@ -57,7 +67,8 @@ else if (pk==2)
 	printf("enter key:\n");
 	scanf(" %d", &key);
 	printf("\n");
-	for(i=0; text[i] != '\0'; ++i) { 
+	for(i=0; text[i] != '\0'; ++i) //for every charactor written it will do the following
+	{ 
 		num = text[i]; 
 		if(num >= 65 && num <= 90) { 
 			num = num - key; 
@@ -75,7 +86,7 @@ else if (pk==2)
 	
 
 
-	printf("your encrypted message is:\n%s\nthe key is \n%d", text, key); 
+	printf("your decrypted message is:\n%s\nthe key is \n%d", text, key); 
 }
 
 	return 0;
