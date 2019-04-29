ff8c8bbaa9db2220e052b0b1f77432bb0e407512
sam osborne <samosborne121@gmail.com>
2019-04-29 04:18:48 +0000

    working rotation

--- file 1.c
+++ file 1.c
@@ -5,29 +5,42 @@
 rotation cipher encryption
 */
 
-int main() 
-{
-char text [] ;
-printf ("enter your message");
-scanf (" %[^/n]", text);
-ptintf("enter key");
-scanf (" %d", &key);
-int i=0
-for(i=0; text != '\0'; i++;)
-{
-    num = text[i];
-    num  =  num + key;
-    if( num > 90)
-        { num = num-26}
-    if ( num < 65);
-        {num = num+26}
-        text[i] = num;
-}
+int main()
 
-    printf ("your key is %d\n your encrypted message is %s", key, text);
-return 0;
-}
+{
+    int pk=0;
+printf ("enter 1 to encrypt roataion cipher with key\n enter 2 to decrypt roatation cyper with key\n");
+scanf ("%d", &pk);
+if (pk=1)
+{
 
-/* 
-rotain cipher decription
-*/
+	unsigned char text[1000]; 
+	int i, key; 
+	char num;
+	
+	
+	printf("enter message:\n");
+	scanf(" %[^\n]s", text); 
+	printf("enter key:\n");
+	scanf(" %d", &key);
+	printf("\n");
+	
+	for(i=0; text[i] != '\0'; ++i) { 
+		num = text[i]; 
+		if(num >= 65 && num <= 90) { 
+			num = num + key; 
+			if(num < 65) { 
+			num = num + 26;
+			}
+			if(num > 90) { 
+				num = num - 26; 
+			}
+			
+			text[i] = num; 
+		}
+	} 
+	
+	printf("your encrypted message is:\n%s\nthe key is \n%d", text, key); 
+	
+	return 0;
+}}
